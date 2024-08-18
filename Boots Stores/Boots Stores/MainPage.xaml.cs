8b503991d05c28aa4019e4aba9fbe8ab4e956d80
Simon Quistelli <simonq_uk@yahoo.com>
2024-08-18 12:17:39 +0100

    add stores VM

--- Boots Stores/Boots Stores/MainPage.xaml.cs
+++ Boots Stores/Boots Stores/MainPage.xaml.cs
@@ -10,15 +10,18 @@ namespace Boots_Stores
 {
     public partial class MainPage : ContentPage
     {
+        private Stores_VM _Stores;
         public MainPage()
         {
+            _Stores = new Stores_VM();
             InitializeComponent();
-            BTN_ShowStore.Clicked += BTN_ShowStore_Clicked;
+            lab_storeNumber.BindingContext = _Stores;
+            btn_ShowStore.Clicked += BTN_ShowStore_Clicked;
         }
 
         private void BTN_ShowStore_Clicked(object sender, EventArgs e)
         {
-            throw new NotImplementedException();
+            lab_StoreName.Text = "Cribbs Causeway";
         }
     }
 }
---
+++ Boots Stores/Boots Stores/Stores_VM.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace Boots_Stores
+{
+    public class Stores_VM : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private int _StoreNumber;
+        private string _StoreName;
+
+        public string StoreNumber
+        {
+            get { return _StoreNumber.ToString(); }
+        }
+
+        public string StoreName
+        {
+            get { return _StoreName; }
+        }
+
+        public Stores_VM()
+        {
+            _StoreNumber = 2089;
+            _StoreName = "";
+        }
+
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
